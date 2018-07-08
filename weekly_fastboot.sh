dfc9aa34170484c4a7a8ee536121cf1aab0c8f79
yshalsager <ysh-alsager@hotmail.com>
2018-07-08 17:53:45 +0200

    script: adapt for fastboot weekly

--- weekly_fastboot.sh
+++ weekly_fastboot.sh
@@ -2,9 +2,9 @@
 rm raw_out compare changes updates dl_links 2> /dev/null
 
 #Check if db exist
-if [ -e stable_fastboot_db ]
+if [ -e weekly_fastboot_db ]
 then
-    mv stable_fastboot_db stable_fastboot_db_old
+    mv weekly_fastboot_db weekly_fastboot_db_old
 else
     echo "DB not found!"
 fi
@@ -14,20 +14,20 @@ echo Fetching updates:
 cat devices | while read device; do
 	codename=$(echo $device | cut -d , -f1)
 	region=$(echo $device | cut -d , -f3)
-	url=`./fastboot.sh $codename F $region`
+	url=`./fastboot.sh $codename X $region`
 	tmpname=$(echo $device | cut -d , -f1 | sed 's/_/-/g')
 	echo $tmpname"="$url >> raw_out
 done
 sed -i 's\http.*com//\not avilable\g' ./raw_out
 sed -i 's/^ *//; s/ *$//; /^$/d' ./raw_out
-cat raw_out | sort | sed 's\http.*/\\' | sed 's/_[0-9]*.[0-9]*.[0-9]*_[0-9]*.[0-9]*_[a-z]*_[a-z0-9]*.[a-z]*//g' | sed 's/-/_/g' > stable_fastboot_db
+cat raw_out | sort | sed 's\http.*/\\' | sed 's/_[0-9]*.[0-9]*.[0-9]*_[0-9]*.[0-9]*_[a-z]*_[a-z0-9]*.[a-z]*//g' | sed 's/-/_/g' > weekly_fastboot_db
 
 #Compare
 echo Comparing:
-cat stable_fastboot_db | while read rom; do
+cat weekly_fastboot_db | while read rom; do
 	codename=$(echo $rom | cut -d = -f1)
-	new=`cat stable_fastboot_db | grep $codename | cut -d = -f2`
-	old=`cat stable_fastboot_db_old | grep $codename | cut -d = -f2`
+	new=`cat weekly_fastboot_db | grep $codename | cut -d = -f2`
+	old=`cat weekly_fastboot_db_old | grep $codename | cut -d = -f2`
 	diff <(echo "$old") <(echo "$new") | grep ^"<\|>" >> compare
 done
 awk '!seen[$0]++' compare > changes
@@ -57,9 +57,8 @@ cat dl_links | while read line; do
 	version=$(echo $line | cut -d = -f2 | cut -d / -f4)
 	android=$(echo $line | cut -d = -f2 | cut -d / -f5 | cut -d _ -f5 | cut -d . -f1,2)
 	link=$(echo $line | cut -d = -f2)
-	./telegram -t $bottoken -c $chat -M "New update available!
-	*Device*: $model
-	*Codename*: $codename
+	./telegram -t $bottoken -c $chat -M "New fastboot image available!
+	*Device*: $codename
 	*Version*: $version
 	*Android*: $android
 	*Download Link*: [Here]($link)
@@ -68,5 +67,5 @@ done
 
 #Push
 git config --global user.email "$gitmail"; git config --global user.name "$gituser"
-git add stable_fastboot_db; git commit -m "Sync: $(date +%d.%m.%Y)"
-git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/XiaomiFirmwareUpdater/miui-updates-tracker.git HEAD:stable_fastboot
+git add weekly_fastboot_db; git commit -m "Sync: $(date +%d.%m.%Y)"
+git push -q https://$GIT_OAUTH_TOKEN_XFU@github.com/XiaomiFirmwareUpdater/miui-updates-tracker.git HEAD:weekly_fastboot
