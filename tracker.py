b2782e3ad5b20c0a32336935e637643770f8e344
yshalsager <ysh-alsager@hotmail.com>
2019-01-02 13:00:30 +0000

    V2.2 * fix tg pushing * fix fastboot codename * underhood changes :p

--- tracker.py
+++ tracker.py
@@ -17,40 +17,55 @@ DISCORD_BOT_TOKEN = environ['DISCORD_BOT_TOKEN']
 channelID = "484478392562089995"
 
 # devices lists
-names = {'aqua': 'Mi 4S China', 'beryllium_global': 'Poco F1 Global',
-         'cactus': 'Redmi 6A China', 'cactus_global': 'Redmi 6A Global', 'cancro': 'Mi 3 - Mi 4 China',
-         'cancro_global': 'Mi 3 - Mi 4 Global', 'cancro_lte_ct': 'Mi 4 LTE-CT', 'cappu': 'Mi Pad 3 China',
-         'capricorn': 'Mi 5s China', 'capricorn_global': 'Mi 5s Global', 'cereus': 'Redmi 6 China',
-         'cereus_global': 'Redmi 6 Global', 'chiron': 'Mi MIX 2 China', 'chiron_global': 'Mi MIX 2 Global',
-         'clover': 'Mi Pad 4 (Plus) China', 'dipper': 'Mi 8 China', 'dipper_global': 'Mi 8 Global',
-         'equuleus': 'Mi 8 Pro China', 'equuleus_global': 'Mi 8 Pro Global',
-         'gemini': 'Mi 5 China', 'gemini_global': 'Mi 5 Global', 'helium': 'Mi Max Prime China',
-         'helium_global': 'Mi Max Prime Global', 'hennessy': 'Redmi Note 3 MTK China', 'hermes': 'Redmi Note 2 China',
-         'hermes_global': 'Redmi Note 2 Global', 'hydrogen': 'Mi Max China', 'hydrogen_global': 'Mi Max Global',
-         'ido_xhdpi': 'Redmi 3 China', 'ido_xhdpi_global': 'Redmi 3 Global', 'jason': 'Mi Note 3 China',
-         'jason_global': 'Mi Note 3 Global', 'kate_global': 'Redmi Note 3 SE Global', 'kenzo': 'Redmi Note 3 China',
-         'kenzo_global': 'Redmi Note 3 Global', 'land': 'Redmi 3S China', 'land_global': 'Redmi 3S Global',
-         'latte': 'Mi Pad 2 China', 'libra': 'Mi 4c China', 'lithium': 'Mi MIX China',
-         'lithium_global': 'Mi MIX Global', 'markw': 'Redmi 4 Prime China', 'markw_global': 'Redmi 4 Prime Global',
-         'meri': 'Mi 5C China', 'mido': 'Redmi Note 4 China', 'mido_global': 'Redmi Note 4 Global',
-         'natrium': 'Mi 5s Plus China', 'natrium_global': 'Mi 5s Plus Global', 'nikel': 'Redmi Note 4 MTK China',
-         'nikel_global': 'Redmi Note 4 MTK Global', 'nitrogen': 'Mi Max 3 China', 'nitrogen_global': 'Mi Max 3 Global',
-         'omega': 'Redmi Pro China', 'oxygen': 'Mi Max 2 China', 'oxygen_global': 'Mi Max 2 Global',
-         'perseus': 'Mi MIX 3 China', 'perseus_global': 'Mi MIX 3 Global', 'platina': 'Mi 8 Lite China',
-         'platina_global': 'Mi 8 Lite Global', 'polaris': 'Mi MIX 2S China', 'polaris_global': 'Mi MIX 2S Global',
-         'prada': 'Redmi 4 China', 'prada_global': 'Redmi 4 Global',
-         'riva': 'Redmi 5A China', 'riva_global': 'Redmi 5A Global', 'rolex': 'Redmi 4A China',
-         'rolex_global': 'Redmi 4A Global', 'rosy': 'Redmi 5 China', 'rosy_global': 'Redmi 5 Global',
-         'sagit': 'Mi 6 China', 'sagit_global': 'Mi 6 Global',
-         'sakura': 'Redmi 6 Pro China', 'sakura_india_global': 'Redmi 6 Pro India', 'santoni': 'Redmi 4X China',
-         'santoni_global': 'Redmi 4X Global', 'scorpio': 'Mi Note 2 China', 'scorpio_global': 'Mi Note 2 Global',
-         'sirius': 'Mi 8 SE China', 'tiffany': 'Mi 5X China', 'tulip_global': 'Redmi Note 6 Pro Global',
-         'ursa': 'Mi 8 Explorer China', 'ugg': 'Redmi Note 5A Prime China', 'ugg_global': 'Redmi Note 5A Prime Global',
-         'ugglite': 'Redmi Note 5A (2GB) China', 'ugglite_global': 'Redmi Note 5A (2GB) Global',
-         'vince': 'Redmi 5 Plus China', 'vince_global': 'Redmi 5 Plus Global - Redmi Note 5 India',
-         'wayne': 'Mi 6X China', 'whyred': 'Redmi Note 5 China',
-         'whyred_global': 'Redmi Note 5 Global - Redmi Note 5 Pro India',
-         'ysl': 'Redmi S2 China', 'ysl_global': 'Redmi S2 Global - Redmi Y2'}
+names = {'aqua': ('Mi 4S China', 'MI4s'), 'beryllium_global': ('Poco F1 Global', 'POCOF1Global'),
+         'cactus': ('Redmi 6A China', 'HM6A'), 'cactus_global': ('Redmi 6A Global', 'HM6AGlobal'),
+         'cancro': ('Mi 3 - Mi 4 China', 'MI3WMI4W'), 'cancro_global': ('Mi 3 - Mi 4 Global', 'MI3WMI4WGlobal'),
+         'cancro_lte_ct': ('Mi 4 LTE-CT', 'MI4LTECT'), 'cappu': ('Mi Pad 3 China', 'MIPAD3'),
+         'capricorn': ('Mi 5s China', 'MI5S'), 'capricorn_global': ('Mi 5s Global', 'MI5SGlobal'),
+         'cereus': ('Redmi 6 China', 'HM6'), 'cereus_global': ('Redmi 6 Global', 'HM6Global'),
+         'chiron': ('Mi MIX 2 China', 'MIMIX2'), 'chiron_global': ('Mi MIX 2 Global', 'MIMIX2Global'),
+         'clover': ('Mi Pad 4 (Plus) China', 'MIPAD4'), 'dipper': ('Mi 8 China', 'MI8'),
+         'dipper_global': ('Mi 8 Global', 'MI8Global'), 'equuleus': ('Mi 8 Pro China', 'MI8UD'),
+         'equuleus_global': ('Mi 8 Pro Global', 'MI8UDGlobal'), 'gemini': ('Mi 5 China', 'MI5'),
+         'gemini_global': ('Mi 5 Global', 'MI5Global'), 'helium': ('Mi Max Prime China', 'MIMAX652'),
+         'helium_global': ('Mi Max Prime Global', 'MIMAX652Global'), 'hennessy': ('Redmi Note 3 MTK China', 'HMNote3'),
+         'hermes': ('Redmi Note 2 China', 'HMNote2'), 'hermes_global': ('Redmi Note 2 Global', 'HMNote2Global'),
+         'hydrogen': ('Mi Max China', 'MIMAX'), 'hydrogen_global': ('Mi Max Global', 'MIMAXGlobal'),
+         'ido_xhdpi': ('Redmi 3 China', 'HM3'), 'ido_xhdpi_global': ('Redmi 3 Global', 'HM3Global'),
+         'jason': ('Mi Note 3 China', 'MINote3'), 'jason_global': ('Mi Note 3 Global', 'MINote3Global'),
+         'kate_global': ('Redmi Note 3 SE Global', 'HMNote3ProtwGlobal'), 'kenzo': ('Redmi Note 3 China', 'HMNote3Pro'),
+         'kenzo_global': ('Redmi Note 3 Global', 'HMNote3ProGlobal'), 'land': ('Redmi 3S China', 'HM3S'),
+         'land_global': ('Redmi 3S Global', 'HM3SGlobal'), 'latte': ('Mi Pad 2 China', 'MIPAD2'),
+         'libra': ('Mi 4c China', 'MI4c'), 'lithium': ('Mi MIX China', 'MIMIX'),
+         'lithium_global': ('Mi MIX Global', 'MIMIXGlobal'), 'markw': ('Redmi 4 Prime China', 'HM4Pro'),
+         'markw_global': ('Redmi 4 Prime Global', 'HM4ProGlobal'), 'meri': ('Mi 5C China', 'MI5C'),
+         'mido': ('Redmi Note 4 China', 'HMNote4X'), 'mido_global': ('Redmi Note 4 Global', 'HMNote4XGlobal'),
+         'natrium': ('Mi 5s Plus China', 'MI5SPlus'), 'natrium_global': ('Mi 5s Plus Global', 'MI5SPlusGlobal'),
+         'nikel': ('Redmi Note 4 MTK China', 'HMNote4'), 'nikel_global': ('Redmi Note 4 MTK Global', 'HMNote4Global'),
+         'nitrogen': ('Mi Max 3 China', 'MIMAX3'), 'nitrogen_global': ('Mi Max 3 Global', 'MIMAX3Global'),
+         'omega': ('Redmi Pro China', 'HMPro'), 'oxygen': ('Mi Max 2 China', 'MIMAX2'),
+         'oxygen_global': ('Mi Max 2 Global', 'MIMAX2Global'), 'perseus': ('Mi MIX 3 China', 'MIMIX3'),
+         'perseus_global': ('Mi MIX 3 Global', 'MIMIX3Global'), 'platina': ('Mi 8 Lite China', 'MI8Lite'),
+         'platina_global': ('Mi 8 Lite Global', 'MI8LiteGlobal'), 'polaris': ('Mi MIX 2S China', 'MIMIX2S'),
+         'polaris_global': ('Mi MIX 2S Global', 'MIMIX2SGlobal'), 'prada': ('Redmi 4 China', 'HM4'),
+         'prada_global': ('Redmi 4 Global', 'HM4Global'), 'riva': ('Redmi 5A China', 'HM5A'),
+         'riva_global': ('Redmi 5A Global', 'HM5AGlobal'), 'rolex': ('Redmi 4A China', 'HM4A'),
+         'rolex_global': ('Redmi 4A Global', 'HM4AGlobal'), 'rosy': ('Redmi 5 China', 'HM5'),
+         'rosy_global': ('Redmi 5 Global', 'HM5Global'), 'sagit': ('Mi 6 China', 'MI6'),
+         'sagit_global': ('Mi 6 Global', 'MI6Global'), 'sakura': ('Redmi 6 Pro China', 'HM6Pro'),
+         'sakura_india_global': ('Redmi 6 Pro India', 'HM6ProINGlobal'), 'santoni': ('Redmi 4X China', 'HM4X'),
+         'santoni_global': ('Redmi 4X Global', 'HM4XGlobal'), 'scorpio': ('Mi Note 2 China', 'MINote2'),
+         'scorpio_global': ('Mi Note 2 Global', 'MINote2Global'), 'sirius': ('Mi 8 SE China', 'MI8SE'),
+         'tiffany': ('Mi 5X China', 'MI5X'), 'tulip_global': ('Redmi Note 6 Pro Global', 'HMNote6ProGlobal'),
+         'ugg': ('Redmi Note 5A Prime China', 'HMNote5A'),
+         'ugg_global': ('Redmi Note 5A Prime Global', 'HMNote5AGlobal'),
+         'ugglite': ('Redmi Note 5A (2GB) China', 'HMNote5ALITE'),
+         'ugglite_global': ('Redmi Note 5A (2GB) Global', 'HMNote5ALITEGlobal'),
+         'ursa': ('Mi 8 Explorer China', 'MI8Explorer'), 'vince': ('Redmi 5 Plus China', 'HM5Plus'),
+         'vince_global': ('Redmi 5 Plus Global - Redmi Note 5 India', 'HM5PlusGlobal'),
+         'wayne': ('Mi 6X China', 'MI6X'), 'whyred': ('Redmi Note 5 China', 'HMNote5'),
+         'whyred_global': ('Redmi Note 5 Global - Redmi Note 5 Pro India', 'HMNote5HMNote5ProGlobal'),
+         'ysl': ('Redmi S2 China', 'HMS2'), 'ysl_global': ('Redmi S2 Global - Redmi Y2', 'HMS2Global')}
 sr_devices = {'aqua': '7.0', 'beryllium_global': '9.0', 'cactus': '8.1',
               'cactus_global': '8.1', 'cancro': '6.0', 'cancro_global': '6.0', 'cancro_lte_ct': '6.0', 'cappu': '7.0',
               'capricorn': '8.0', 'capricorn_global': '7.0', 'cereus': '8.1', 'cereus_global': '8.1', 'chiron': '8.0',
@@ -177,15 +192,14 @@ for v in versions:
     if changes is True:
         # Notify
         rom = str(v.replace('_', ' '))
-        if "_recovery" in v:
-            for key, value in c.items():
-                if "Global" in str(value['filename']):
-                    region = "Global"
-                else:
-                    region = "China"
+        for key, value in c.items():
+            if "_recovery" in v:
                 android = str(value['filename']).split('_')[4].replace('.zip', ' ')
-                codename = value['codename']
-                device = value['device']
+                product = str(value['filename']).split('_')[1]
+                for c, info in names.items():
+                    if info[1] == product:
+                        codename = str(c).split('_')[0]
+                        device = info[0]
                 version = value['version']
                 link = value['download']
                 telegram_message = "New {0} update available!: \n" \
@@ -204,14 +218,16 @@ for v in versions:
                                   "**Download**: {5} \n" \
                                   "~~                                                     ~~" \
                     .format(rom, device, codename, version, android, link)
-        elif "_fastboot" in v:
-            for key, value in c.items():
+            elif "_fastboot" in v:
                 if "_cn_" in str(value['filename']):
                     android = str(value['filename']).split('_')[4]
+                    code = str(value['filename']).split('_')[0]
                 elif "_global_" in str(value['filename']):
                     android = str(value['filename']).split('_')[5]
-                device = str(value['filename']).split('_')[0]
-                codename = value['codename']
+                    code = str(value['filename']).split('_images')[0]
+
+                device = names[code][0]
+                codename = str(value['filename']).split('_')[0]
                 version = value['version']
                 md5 = value['md5']
                 link = value['download']
@@ -234,36 +250,35 @@ for v in versions:
                                   "~~                                                     ~~" \
                     .format(rom, device, codename, version, android, md5, link)
 
-        params = (
-            ('chat_id', telegram_chat),
-            ('text', telegram_message),
-            ('parse_mode', "Markdown"),
-            ('disable_web_page_preview', "yes")
-        )
-        telegram_url = "https://api.telegram.org/bot" + bottoken + "/sendMessage"
-        telegram_req = post(telegram_url, params=params)
-        telegram_status = telegram_req.status_code
-        if telegram_status == 200:
-            print("{0}: Telegram Message sent".format(device))
-        else:
-            print("Telegram Error")
+            params = (
+                ('chat_id', telegram_chat),
+                ('text', telegram_message),
+                ('parse_mode', "Markdown"),
+                ('disable_web_page_preview', "yes")
+            )
+            telegram_url = "https://api.telegram.org/bot" + bottoken + "/sendMessage"
+            telegram_req = post(telegram_url, params=params)
+            telegram_status = telegram_req.status_code
+            if telegram_status == 200:
+                print("{0}: Telegram Message sent".format(device))
+            else:
+                print("Telegram Error")
 
-        discord_url = "https://discordapp.com/api/channels/{}/messages".format(channelID)
-        headers = {"Authorization": "Bot {}".format(DISCORD_BOT_TOKEN),
-                   "User-Agent": "myBotThing (http://some.url, v0.1)",
-                   "Content-Type": "application/json", }
-        data = json.dumps({"content": discord_message})
-        discord_req = post(discord_url, headers=headers, data=data)
-        discord_status = discord_req.status_code
-        if discord_status == 200:
-            print("{0}: Discord Message sent".format(device))
-        else:
-            print("Discord Error")
-        if path.exists(v + '/' + 'changes'):
-            remove(v + '/' + 'changes')
+            discord_url = "https://discordapp.com/api/channels/{}/messages".format(channelID)
+            headers = {"Authorization": "Bot {}".format(DISCORD_BOT_TOKEN),
+                       "User-Agent": "myBotThing (http://some.url, v0.1)",
+                       "Content-Type": "application/json", }
+            data = json.dumps({"content": discord_message})
+            discord_req = post(discord_url, headers=headers, data=data)
+            discord_status = discord_req.status_code
+            if discord_status == 200:
+                print("{0}: Discord Message sent".format(device))
+            else:
+                print("Discord Error")
+            if path.exists(v + '/' + 'changes'):
+                remove(v + '/' + 'changes')
     elif changes is False:
         print(v + ": Nothing to do!")
-
 
 # push
 if changes is True:
@@ -273,5 +288,5 @@ if changes is True:
            ""git push -q https://{1}@github.com/XiaomiFirmwareUpdater/miui-updates-tracker.git HEAD:master"
            .format(today, GIT_OAUTH_TOKEN))
 elif changes is False:
-        print("Nothing to push!")
+    print("Nothing to push!")
 
