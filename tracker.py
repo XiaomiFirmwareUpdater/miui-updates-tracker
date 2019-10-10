146f79c8da669706601050a076aa5f4a26b27389
yshalsager <ysh-alsager@hotmail.com>
2019-10-10 16:52:43 +0200

    message: add ID region
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- tracker.py
+++ tracker.py
@@ -146,6 +146,8 @@ def generate_message(update: dict):
     branch = 'Stable' if 'V' in version else 'Weekly'
     if 'eea_global' in filename or 'eea_global' in codename or 'EU' in version:
         region = 'EEA'
+    elif 'id_global' in filename or 'id_global' in codename or 'ID' in version:
+        region = 'India'
     elif 'in_global' in filename or 'in_global' in codename or 'IN' in version:
         region = 'India'
     elif 'ru_global' in filename or 'ru_global' in codename or 'RU' in version:
