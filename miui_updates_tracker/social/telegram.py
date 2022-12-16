0ae690fb680f9d21d99c92d62d5687393b392239
yshalsager <ysh-alsager@hotmail.com>
2022-12-17 01:01:44 +0200

    social: telegram: Handle RetryAfter exceptions
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- miui_updates_tracker/social/telegram.py
+++ miui_updates_tracker/social/telegram.py
@@ -6,12 +6,13 @@ from typing import List, Union
 from urllib.parse import quote
 
 from humanize import naturalsize
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.error import BadRequest, RetryAfter
+from telegram.ext import Updater
+
 from miui_updates_tracker.common.constants import website
 from miui_updates_tracker.common.database.database import get_incremental, get_full_name
 from miui_updates_tracker.common.database.models.miui_update import Update
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.error import BadRequest
-from telegram.ext import Updater
 
 
 class TelegramBot:
@@ -43,7 +44,9 @@ class TelegramBot:
             try:
                 await self.send_telegram_message(message, button)
             except BadRequest:
-                self._logger.warning(f"Can't send telegram message of update {update}.\n Message:{message}")
+                self._logger.warning(
+                    f"Can't send telegram message of update {update}.\n Message:{message}"
+                )
 
     def generate_message(self, update: Update) -> (str, InlineKeyboardMarkup):
         """
@@ -52,10 +55,10 @@ class TelegramBot:
         :return: A string containing the update's message
          and inline keyboard that has download link'
         """
-        short_codename = update.codename.split('_')[0]
+        short_codename = update.codename.split("_")[0]
         message: str = f"New update available!"
         if update.method == "Fastboot":
-            message += '\n'
+            message += "\n"
         else:
             if self.source == "website":
                 message += " (on the official website)\n"
@@ -63,45 +66,68 @@ class TelegramBot:
                 message += " (via OTA)\n"
             else:
                 message += "\n"
-        message += f"*Device*: {get_full_name(update.codename)}\n" \
-                   f"*Codename*: #{short_codename}\n" \
-                   f"*Type*: {update.branch} {update.method}\n" \
-                   f"*Version*: `{update.version} | {update.android}`\n" \
-                   f"*Size*: {naturalsize(update.size)}\n"
+        message += (
+            f"*Device*: {get_full_name(update.codename)}\n"
+            f"*Codename*: #{short_codename}\n"
+            f"*Type*: {update.branch} {update.method}\n"
+            f"*Version*: `{update.version} | {update.android}`\n"
+            f"*Size*: {naturalsize(update.size)}\n"
+        )
         if update.md5:
             message += f"*MD5*: `{update.md5}`\n"
         if update.changelog != "Bug fixes and system optimizations.":
             if len(update.changelog) + len(message) > 4000:
                 branch = quote(update.branch.lower())
-                message += f"*Changelog*: [Here]({website}/miui/{short_codename}/" \
-                           f"{branch}/{update.version}/)\n"
+                message += (
+                    f"*Changelog*: [Here]({website}/miui/{short_codename}/"
+                    f"{branch}/{update.version}/)\n"
+                )
             else:
                 message += f"*Changelog*:\n`{update.changelog.replace('[', '(').replace(']', ')')}`\n"
         message += "\n@MIUIUpdatesTracker | @XiaomiFirmwareUpdater"
         button: InlineKeyboardButton = InlineKeyboardButton("Full ROM", update.link)
         # bot subscribe
         subscribe_command = str(
-            b64encode(bytes(f"/subscribe miui {short_codename}", encoding='utf-8')), encoding='utf-8')
-        more_buttons = [InlineKeyboardButton("Latest", f"{website}/miui/{short_codename}"),
-                        InlineKeyboardButton("Archive", f"{website}/archive/miui/{short_codename}"),
-                        InlineKeyboardButton("Subscribe", f"https://t.me/XiaomiGeeksBot?start={subscribe_command}")]
+            b64encode(bytes(f"/subscribe miui {short_codename}", encoding="utf-8")),
+            encoding="utf-8",
+        )
+        more_buttons = [
+            InlineKeyboardButton("Latest", f"{website}/miui/{short_codename}"),
+            InlineKeyboardButton("Archive", f"{website}/archive/miui/{short_codename}"),
+            InlineKeyboardButton(
+                "Subscribe", f"https://t.me/XiaomiGeeksBot?start={subscribe_command}"
+            ),
+        ]
         # incremental update
         if update.method == "Recovery":
             incremental = get_incremental(update.version)
             if incremental:
                 incremental_button: InlineKeyboardButton = InlineKeyboardButton(
-                    "Incremental", incremental.link)
-                return message, InlineKeyboardMarkup([[button, incremental_button], more_buttons])
+                    "Incremental", incremental.link
+                )
+                return message, InlineKeyboardMarkup(
+                    [[button, incremental_button], more_buttons]
+                )
         return message, InlineKeyboardMarkup([[button], more_buttons])
 
-    async def send_telegram_message(self, message: str, reply_markup: InlineKeyboardMarkup):
+    async def send_telegram_message(
+            self, message: str, reply_markup: InlineKeyboardMarkup
+    ):
         """
         Send a message to Telegram chat
         :param message: A string of the update message to be sent
         :param reply_markup: A inline keyboard markup object that contains the update list
         :return:
         """
-        self.updater.bot.send_message(chat_id=self.chat, text=message,
-                                      parse_mode='Markdown', disable_web_page_preview='yes',
-                                      reply_markup=reply_markup)
+        try:
+            self.updater.bot.send_message(
+                chat_id=self.chat,
+                text=message,
+                parse_mode="Markdown",
+                disable_web_page_preview="yes",
+                reply_markup=reply_markup,
+            )
+        except RetryAfter as error:
+            await sleep(error.retry_after)
+            return await self.send_telegram_message(message, reply_markup)
         await sleep(3)
