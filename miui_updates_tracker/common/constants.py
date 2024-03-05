9071b2aa947a3a6164ed67e1e814c0c0f02e1fc7
yshalsager <ysh-alsager@hotmail.com>
2024-03-05 18:08:10 +0200

    chore: update android one codenames
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- miui_updates_tracker/common/constants.py
+++ miui_updates_tracker/common/constants.py
@@ -1,4 +1,17 @@
-android_one_devices = ['laurel_sprout_global', 'laurel_sprout_eea_global',
-                       'jasmine_global', 'daisy_global', 'tissot_global', 'tissot',
-                       'tiare_global', 'tiare_in_global', 'tiare_eea_global', 'tiare_ru_global']
+android_one_devices = [
+    "laurel_sprout_global",
+    "laurel_sprout_eea_global",
+    "jasmine_global",
+    "daisy_global",
+    "tissot_global",
+    "tissot",
+    "tiare_global",
+    "tiare_in_global",
+    "tiare_eea_global",
+    "tiare_ru_global",
+    "blue_eea_global",
+    "blue_in_global",
+    "blue_id_global",
+    "blue_ru_global",
+]
 website = "https://xiaomifirmwareupdater.com"
