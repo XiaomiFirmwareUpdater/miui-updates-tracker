8ee58a1df26a865ad6d8afc65e83916b383ce067
yshalsager <ysh-alsager@hotmail.com>
2024-06-03 13:49:02 +0300

    social(XDA): update threads, template and add new devices
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- miui_updates_tracker/social/xda.py
+++ miui_updates_tracker/social/xda.py
@@ -12,7 +12,11 @@ from aiohttp.client_exceptions import ServerDisconnectedError
 from humanize import naturalsize
 
 from miui_updates_tracker.common.constants import website
-from miui_updates_tracker.common.database.database import get_device_roms, get_full_name, get_incremental
+from miui_updates_tracker.common.database.database import (
+    get_device_roms,
+    get_full_name,
+    get_incremental,
+)
 from miui_updates_tracker.common.database.models.miui_update import Update
 from miui_updates_tracker.social.xda_poster.xda import XDA
 
@@ -23,11 +27,11 @@ class XDAPoster(XDA):
     """XDA API client"""
 
     def __init__(self, api_key):
-        with open(f'{current_dir}/xda_threads.yml', 'r') as file:
+        with open(f"{current_dir}/xda_threads.yml", "r") as file:
             self.threads = yaml.load(file, Loader=yaml.CLoader)
-        with open(f'{current_dir}/xda_update_template.txt', 'r') as template:
+        with open(f"{current_dir}/xda_update_template.txt", "r") as template:
             self.update_template: Template = Template(template.read())
-        with open(f'{current_dir}/xda_thread_template.txt', 'r') as template:
+        with open(f"{current_dir}/xda_thread_template.txt", "r") as template:
             self.thread_template: Template = Template(template.read())
         super().__init__(api_key)
 
@@ -37,7 +41,7 @@ class XDAPoster(XDA):
         :param update: an Update object that contains update's information from official website
         :return: A string containing the update's message
         """
-        short_codename = update.codename.split('_')[0]
+        short_codename = update.codename.split("_")[0]
         # incremental update
         incremental = None
         if update.method == "Recovery":
@@ -53,8 +57,11 @@ class XDAPoster(XDA):
             zip_size=naturalsize(update.size),
             md5_hash=update.md5 if update.md5 else "Unknown",
             link=update.link,
-            incremental_link=f'[URL="{incremental.link}"]Here[/URL]' if incremental else "Unavailable",
-            changelog=update.changelog
+            incremental_link=f'[URL="{incremental.link}"]Here[/URL]'
+            if incremental
+            else "Unavailable",
+            changelog=update.changelog,
+            os_type='hyperos' if update.version.startswith('OS') else 'miui'
         )
 
     def generate_thread(self, codename: str) -> str:
@@ -63,24 +70,33 @@ class XDAPoster(XDA):
         :param codename: devices codename
         :return: A string containing the update's message
         """
-        updates = get_device_roms(codename)
-        grouped_by_name = [list(item) for _, item in
-                           groupby(sorted(updates, key=lambda x: x.name), lambda x: x.name)]
+        updates = list(filter(lambda x: x.date, get_device_roms(codename)))
+        grouped_by_name = [
+            list(item)
+            for _, item in groupby(
+                sorted(updates, key=lambda x: x.name), lambda x: x.name
+            )
+        ]
         updates_history = ""
         for group in grouped_by_name:
             updates_history += f"[B]{group[0].name}[/B]\n"
             updates_history += "[LIST]\n"
             for update in group:
-                updates_history += f"[*]{update.date} | {update.branch} {update.method} | {update.version} | " \
-                                   f"[URL='{update.link}']Download[/URL] | " \
-                                   f"[URL='{website}/miui/{codename}/{quote(update.branch.lower())}/" \
-                                   f"{update.version}/']Details[/URL]\n"
+                updates_history += (
+                    f"[*]{update.date} | {update.branch} {update.method} | {update.version} | "
+                    f"[URL='{update.link}']Download[/URL] | "
+                    f"[URL='{website}/{'hyperos' if update.version.startswith('OS') else 'miui'}/{codename}/{quote(update.branch.lower())}/"
+                    f"{update.version}/']Details[/URL]\n"
+                )
             updates_history += "[/LIST]\n"
-        device_name: str = ' / '.join(list(set([' '.join(i.name.split(' ')[:-1]) for i in updates])))
-        thread = self.thread_template.substitute(codename=codename, device_name=device_name,
-                                                 updates_history=updates_history)
-        if len(thread) > 60000:
-            thread = re.sub(r'\[\*\]201(?:8|9).*\n', '', thread)
+        device_name: str = " / ".join(
+            list(set([" ".join(i.name.split(" ")[:-1]) for i in updates]))
+        )
+        thread = self.thread_template.substitute(
+            codename=codename, device_name=device_name, updates_history=updates_history
+        )
+        if len(thread) > 100500:
+            thread = re.sub(r"\[\*\]20(?:18|19|20|21).*\n", "", thread)
         return thread
 
     async def post_updates(self, new_updates: List[Update]):
@@ -91,22 +107,22 @@ class XDAPoster(XDA):
         """
         # new_updates = sorted(new_updates, key=lambda x: x.date, reverse=True)[:1]
         for update in new_updates:
-            codename = update.codename.split('_')[0]
+            codename = update.codename.split("_")[0]
             if codename not in self.threads.keys():
                 continue
             xda_post = self.generate_message(update)
             try:
-                await self.post_async(self.threads[codename]['thread'], xda_post)
+                await self.post_async(self.threads[codename]["thread"], xda_post)
                 await asyncio.sleep(15)
             except ServerDisconnectedError:
                 logging.error(f"Server disconnected while posting {update}.")
-        updated_devices = list(set([i.codename.split('_')[0] for i in new_updates]))
+        updated_devices = list(set([i.codename.split("_")[0] for i in new_updates]))
         for device in updated_devices:
             if device not in self.threads.keys():
                 continue
             xda_thread = self.generate_thread(device)
             try:
-                await self.update_post_async(self.threads[device]['post'], xda_thread)
+                await self.update_post_async(self.threads[device]["post"], xda_thread)
                 await asyncio.sleep(15)
             except ServerDisconnectedError:
                 logging.error(f"Server disconnected while updating {device} thread.")
@@ -117,17 +133,29 @@ async def main():
     from miui_updates_tracker import CONFIG
     from miui_updates_tracker.common.database import close_db
     from miui_updates_tracker.common.database.database import get_device_latest
-    all_updates = list(filter(lambda x: x.date, get_device_latest('odin')))
-    xda = XDAPoster(CONFIG['xda']['access_token'])
+
+    all_updates = list(filter(lambda x: x.date, get_device_latest("odin")))
+    xda = XDAPoster(CONFIG["xda"]["access_token"])
     await xda.post_updates(all_updates)
     close_db()
 
 
-def run():
-    """asyncio trigger function"""
-    event_loop = asyncio.get_event_loop()
-    event_loop.run_until_complete(main())
+async def update_all_threads():
+    from miui_updates_tracker import CONFIG
+    from miui_updates_tracker.common.database import close_db
+
+    xda = XDAPoster(CONFIG["xda"]["access_token"])
+    for idx, codename in enumerate(xda.threads.keys()):
+        xda_thread = xda.generate_thread(codename)
+        try:
+            await xda.update_post_async(xda.threads[codename]["post"], xda_thread)
+            print(f"Thread updated for {idx} {codename}.")
+            await asyncio.sleep(15)
+        except ServerDisconnectedError:
+            logging.error(f"Server disconnected while updating {codename} thread.")
+    close_db()
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    asyncio.run(main())
+    # asyncio.run(update_all_threads())
